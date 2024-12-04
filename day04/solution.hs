5c4d5722cf5f1d2130187048b02e0937fff445ae
Ben Anhalt <ben.anhalt@gmail.com>
2024-12-04 15:35:55 -0600

    Improve day 4.

--- day04/solution.hs
+++ day04/solution.hs
@@ -1,32 +1,30 @@
-import Data.Map.Strict as M
+import Data.Map.Strict qualified as M
 
 type Grid a = M.Map (Int, Int) a
 
 countXmass :: Grid Char -> Int
-countXmass grid =
-  let ((maxRow, maxCol), _) = M.findMax grid
-   in sum [xmassAt grid r c | r <- [0 .. maxRow], c <- [0 .. maxCol]]
+countXmass grid = sum $ xmassAt grid <$> M.keys grid
 
-xmassAt :: Grid Char -> Int -> Int -> Int
-xmassAt grid r c =
+xmassAt :: Grid Char -> (Int, Int) -> Int
+xmassAt grid (r, c) =
   let dirs = [(dr, dc) | dr <- [-1 .. 1], dc <- [-1 .. 1], (dr, dc) /= (0, 0)]
-   in sum [1 | dir <- dirs, xmasInDir grid r c dir]
+   in length $ filter (xmasInDir grid r c) dirs
 
 xmasInDir :: Grid Char -> Int -> Int -> (Int, Int) -> Bool
 xmasInDir grid r c (dr, dc) =
   let steps = [(r + i * dr, c + i * dc) | i <- [0 .. 3]]
    in (Just "XMAS" ==) $ mapM (`M.lookup` grid) steps
 
-isCross :: Grid Char -> Int -> Int -> Bool
-isCross grid r c =
-  let a = mapM (`M.lookup` grid) [(r + dr, c + dc) | (dr, dc) <- [(-1, -1), (0, 0), (1, 1)]]
-      b = mapM (`M.lookup` grid) [(r + dr, c + dc) | (dr, dc) <- [(1, -1), (0, 0), (-1, 1)]]
+isCross :: Grid Char -> (Int, Int) -> Bool
+isCross grid (r, c) =
+  let
+    strAlong steps = mapM (`M.lookup` grid) [(r + dr, c + dc) | (dr, dc) <- steps]
+    a = strAlong [(-1, -1), (0, 0), (1, 1)]
+    b = strAlong [(1, -1), (0, 0), (-1, 1)]
    in ((a == Just "MAS") || (a == Just "SAM")) && ((b == Just "MAS") || (b == Just "SAM"))
 
 countCrosses :: Grid Char -> Int
-countCrosses grid =
-  let ((maxRow, maxCol), _) = M.findMax grid
-   in sum [1 | r <- [0 .. maxRow], c <- [0 .. maxCol], isCross grid r c]
+countCrosses grid = length $ filter (isCross grid) $ M.keys grid
 
 main :: IO ()
 main = do
